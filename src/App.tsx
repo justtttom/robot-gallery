4b72fc729f5142a3e2759bd45a2e22a5e2b7fa0f
Justin Tom <2541198816@qq.com>
2022-08-19 20:46:51 +0800

    say what?

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,33 @@ import robots from './mockdata/robots.json';
 import Robot from "./components/Robot";
 import styles from './App.module.css';
 import ShoppingCart from './components/ShoppingCart';
+import { log } from 'console';
 
 interface Props { }
 
 interface State {
-  robotGallery: any[]
+  robotGallery: any[];
+  count: number;
 }
 
 
 class App extends React.Component<Props, State> {
+
+  // 生命周期第一阶段： 初始化
+  // 初始化组件 state
   constructor(props) {
     super(props);
     this.state = {
       robotGallery: [],
+      count: 0,
     };
   }
 
+  // 在组件创建好dom元素以后、挂在页面时调用
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(resopnse => resopnse.json())
-      .then(data => this.setState({robotGallery:data}));
+      .then(data => this.setState({ robotGallery: data }));
   }
 
 
@@ -34,6 +41,15 @@ class App extends React.Component<Props, State> {
           <img src={logo} className={styles.appLogo} alt="logo" />
           <h1>hahaha... hello world</h1>
         </div>
+        <button onClick={() => {
+          this.setState((preState, preProps) => { return { count: preState.count + 1 } }, () => {
+            console.log(this.state.count);
+          });
+          this.setState((preState, preProps) => { return { count: preState.count + 1 } }, () => {
+            console.log(this.state.count);
+          });
+        }}>hey</button>
+        <span>count:{this.state.count}</span>
         <ShoppingCart />
         <div className={styles.robotList}>
           {this.state.robotGallery.map(r => <Robot id={r.id} email={r.email} name={r.name} />)}
