88b9abd0cbe1bd9c467c50a2e7c04e0b83f33ca8
Justin Tom <2541198816@qq.com>
2022-08-22 21:26:51 +0800

    big change

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React,{useContext} from "react";
+import {appSetStateContext} from "../AppState";
+import {RobotProps} from "./Robot"
 
-const withAddToCart = (ChildComponent:React.ComponentType) => {
-  // return class extends React.Component{}
-  return (props) => {
-    
-  };
+export const withAddToCart = (ChildComponent: React.ComponentType<RobotProps>) => {
+    return (props) => {
+        const setState = useContext(appSetStateContext);
+        const addToCart = (id,name) => {
+            if (setState) {//思考：同学们想一想如何简化这里的代码
+                setState(state => {
+                    return {
+                        ...state,
+                        shoppingCart: {
+                            items: [...state.shoppingCart.items, { id, name }]
+                        }
+                    }
+                })
+            }
+        }
+        return <ChildComponent {...props}  addToCart={addToCart}/>
+    };
 }
--- src/components/RobotDiscount.tsx
+++ src/components/RobotDiscount.tsx
@@ -1,37 +1,29 @@
-import React, { useContext } from "react";
-import styles from './Robot.module.css'
-import { appContext, appSetStateContext } from '../AppState'
+import React, { useContext, useState } from "react";
+import styles from "./Robot.module.css";
+import { appContext, appSetStateContext } from "../AppState";
+import {withAddToCart} from "./AddToCart";
+
+
 interface RobotProps {
-  id: number,
-  name: string,
-  email: string
+    id: number;
+    name: string;
+    email: string;
+    addToCart:(id,name)=>void;
 }
 
-const RobotDiscount: React.FC<RobotProps> = ({ id, name, email }) => {
-  const value = useContext(appContext);
-  const setState = useContext(appSetStateContext);
-  const addToCart = () => {
-    if (setState) {
-      setState(state => {
-        return {
-          ...state,
-          shoppingCart: {
-            items: [...state.shoppingCart.items, { id, name }]
-          }
-        }
-      })
-    }
-  }
-  return (
-    <div className={styles.cardContainer}>
-      <img src={`https://robohash.org/${id}`} alt="robot" />
-      <h2>打折商品</h2>
-      <h2>{name}</h2>
-      <p>{email}</p>
-      <p>作者：{value.username}</p>
-      <button onClick={addToCart}>加入购物车</button>
-    </div>
-  )
-}
+const RobotDiscount: React.FC<RobotProps> = ({ id, name, email,addToCart }) => {
+    const value = useContext(appContext)
+    
+    return (
+        <div className={styles.cardContainer}>
+            <img alt="robot" src={`https://robohash.org/${id}`} />
+            <h2>打折商品</h2>
+            <h2>{name}</h2>
+            <p>{email}</p>
+            <p>作者：{value.username}</p>
+            <button onClick={()=>addToCart(id,name)}>加入购物车</button>
+        </div>
+    );
+};
 
-export default RobotDiscount;
+export default withAddToCart(RobotDiscount);
